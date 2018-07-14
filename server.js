382c93e3deebcf6e6867481a0a2800f9c7d3586e
Dally Wally <dallin.watac.major@gmail.com>
2018-07-14 10:27:04 -0600

    saved and formated

--- server.js
+++ server.js
@@ -5,26 +5,26 @@ var app = express();
 
 var PORT = process.env.PORT || 3307;
 
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
-  });
-  
-  app.get("/reserved", function(req, res) {
+});
+
+app.get("/reserved", function (req, res) {
     res.sendFile(path.join(__dirname, "reserved.html"));
-  });
+});
 
-  app.get("/tables", function(req, res) {
+app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
-  });
+});
 
-  
+
 
 require(".routes/apiRoutes")(app);
 require(".routes.htmlRoutes")(app);
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
 
