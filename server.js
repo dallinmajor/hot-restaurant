8588d972f2352572f74d368920105b6fff776a00
Dally Wally <dallin.watac.major@gmail.com>
2018-07-14 10:57:40 -0600

    added a table js amount other things

--- server.js
+++ server.js
@@ -7,19 +7,6 @@ var PORT = process.env.PORT || 3307;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-});
-
-app.get("/reserved", function (req, res) {
-    res.sendFile(path.join(__dirname, "reserved.html"));
-});
-
-app.get("/tables", function (req, res) {
-    res.sendFile(path.join(__dirname, "tables.html"));
-});
-
-
 
 require(".routes/apiRoutes")(app);
 require(".routes.htmlRoutes")(app);
